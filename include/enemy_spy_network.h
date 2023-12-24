8ca9121cb1bef1b89388d4f35901e8d1e0e98d1c
Elounelli Aouj <aouj.elounelli@ecole.ensicaen.fr>
2023-12-24 12:31:23 +0100

    Update enemy_spy_network.h

--- include/enemy_spy_network.h
+++ include/enemy_spy_network.h
@@ -1,63 +1,46 @@
 #ifndef ENEMY_SPY_NETWORK_H
 #define ENEMY_SPY_NETWORK_H
 
-#include <pthread.h>
-
-#define MAX_AGENTS 4
-#define MAX_BUILDINGS
-#define MAX_CITIZENS
+#include "memory.h"
+#include "monitor.h"
+#include "enemy_spy_network.h"
 
 
-// Define the structure for agents (spies and officer)
-struct Agent {
-    int id;                 // Unique identifier for the agent
-    int type;               // 0 for citizen, 1 for officer traitant, 2 for agent source
-    int hasLicense;         // 1 if the agent has a license to kill, 0 otherwise
-    int position[2]         // coordinates in the city grid
-    int health;             // Current health points
-    int maxHealth;          // Maximum health points
-    int state;              // 0 for healthy, 1 for injured, 2 for dead
-    int remainingRest;      // Remaining rest rounds needed for recovery
-    int currentBuilding;    // ID of the current building (if inside), -1 otherwise
-    // Other necessary fields according to the requirements
-};
+int spy_choice(spy_t spy);
 
-// Define the structure for other inhabitants (citizens)
-struct Citizen {
-    int id;                 // Unique identifier for the citizen
-    int positionX;          // X-coordinate in the city grid
-    int positionY;          // Y-coordinate in the city grid
-    int workplace;          // ID of the workplace (Hôtel de Ville, Supermarket, or Company)
-    // Other necessary fields according to the requirements
-};
+/* Return 1 if the spy already stolen this company */
+int already_stolen(spy_t spy);
 
-// Define the structure for buildings
-struct Building {
-    int type;               // Type of the building (Hôtel de Ville, Supermarket, Company, etc.)
-    int capacity;           // Maximum number of characters the building can hold
-    // Other necessary fields according to the requirements
-};
+/* Return 1 if someone already stolen this company */
+int someone_stolen(int row, int column);
 
-// Define the structure for the city
-struct City {
-    int sizeX;              // Size of the city grid in the X-direction
-    int sizeY;              // Size of the city grid in the Y-direction
-    // Other necessary fields according to the requirements
-};
+/* Return 1 if the spy steal the company */
+int possible_theft(spy_t spy, int t);
 
-// Define the structure for shared memory
-struct SharedMemory {
-    struct Agent agents[MAX_AGENTS];          // Array of agents
-    struct Citizen citizens[MAX_CITIZENS];    // Array of citizens
-    struct Building buildings[MAX_BUILDINGS]; // Array of buildings
-    struct City city;                         // City information
-    // Other necessary fields according to the requirements
-};
+void check_position(memory_t* memory, int choice);
+
+void spy_move_around_company(spy_t* spy, cell_t pos);
+
+/* Return 1 if the spy wants to steal the company */
+int consider_theft(spy_t* spy, int* pos);
+
+void theft(spy_t* spy);
+
+void move_spy(spy_t* spy);
+
+spyInfo* get_info_spy(int is_killer, int id);
+
+caseOfficerInfo* get_info_case_officer();
+
+counterIntelligenceOfficer* get_info_counter_intelligence_officer();
+
+void fill_company_spy(memory_t* memory, int n);
+
+void go_to_mail_box(memory_t* memory, int choice);
+
+int* is_around(int row, int column, int cell_type);
 
 
+#endif /* ENEMY_SPY_NETWORK_H */
 
-// Function prototypes
-void* agentSourceThread(void* arg);
-void* officerTraitantThread(void* arg);
 
-#endif // ENEMY_SPY_NETWORK_H
